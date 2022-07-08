9e37b9d26c4cdd8a3946e6b98d1f56ea20e1946d
Emilio Eguia <emilio.eguia@gmail.com>
2022-07-08 15:00:42 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ZonapropCABA
+ Analysis of Data from 150000+ properties listed in zonaprop.com from Ciudad Autonoma de Buenos Aires (CABA)
---
+++ SQLQueryZonaprop.sql
@@ -0,0 +1,96 @@
+USE zonaprop;
+
+SELECT *
+FROM zonaprop_data;
+
+
+-- Seeing the average price of BUYING a property in each CABA neighborhood
+
+SELECT l3, AVG(price) AS AVGprice 
+FROM zonaprop_data
+WHERE operation_type = 'Venta' AND l3 IS NOT NULL
+GROUP BY l3
+HAVING AVG(price) IS NOT NULL 
+ORDER BY AVGprice;
+
+-- Seeing the average price of BUYING a living property in each CABA neighborhood -- H5
+
+SELECT l3, AVG(price) AS AVGprice, COUNT(l3) AS count, AVG(surface_total) AS surface
+FROM zonaprop_data
+WHERE operation_type = 'Venta' 
+AND l3 IS NOT NULL 
+AND property_type LIKE('Departamento') OR property_type LIKE('PH') OR property_type LIKE('Casa') 
+GROUP BY l3
+HAVING AVG(price) IS NOT NULL 
+ORDER BY AVGprice;
+
+-- Seeing the average price of RENTING a living property in each CABA neighborhood -- H5
+
+SELECT l3, AVG(price) AS AVGprice, COUNT(l3) AS count, AVG(surface_total) AS surface
+FROM zonaprop_data
+WHERE operation_type = 'Alquiler' 
+AND l3 IS NOT NULL 
+AND property_type LIKE('Departamento') OR property_type LIKE('PH') OR property_type LIKE('Casa') 
+GROUP BY l3
+HAVING AVG(price) IS NOT NULL 
+ORDER BY AVGprice;
+
+
+-- Seeing the average price of RENTING a FLAT in each CABA neighborhood -- H6?
+
+SELECT l3, AVG(price) AS AVGprice, COUNT(l3) AS count, AVG(surface_total) AS surface
+FROM zonaprop_data
+WHERE operation_type = 'Alquiler' 
+AND l3 IS NOT NULL 
+AND property_type LIKE('Departamento')
+GROUP BY l3
+HAVING AVG(price) IS NOT NULL 
+ORDER BY AVGprice;
+
+-- Seeing RENTING vs SELLING vs TEMPORAL RENTING in each neighborhood -- H2
+
+SELECT operation_type, COUNT(operation_type) AS op_type_count, l3
+FROM zonaprop_data
+WHERE l3 IS NOT NULL
+GROUP BY l3, operation_type
+ORDER BY l3, op_type_count;
+
+-- Seeing the price per sqMeter in selling properties per neighborhood -- H3
+
+SELECT (SUM(price)/SUM(surface_total)) AS price_surface_ratio, l3
+FROM zonaprop_data
+WHERE operation_type = 'Venta' 
+AND l3 IS NOT NULL
+AND surface_total <> 0
+AND price <> 0
+AND property_type LIKE('Departamento') OR property_type LIKE('PH') OR property_type LIKE('Casa') 
+GROUP BY l3
+ORDER BY price_surface_ratio DESC;
+
+-- Average sqMeter price in CABA --H6
+SELECT (SUM(price)/SUM(surface_total)) AS price_surface_ratio
+FROM zonaprop_data
+WHERE operation_type = 'Venta' 
+--AND property_type LIKE('Departamento') OR property_type LIKE('PH') OR property_type LIKE('Casa') 
+
+-- Neighborhoods with more offer -- H4
+SELECT l3, COUNT(l3) AS count_per_neighborhood
+FROM zonaprop_data
+WHERE l3 IS NOT NULL
+GROUP BY l3
+ORDER BY count_per_neighborhood DESC
+
+
+-- Seeing the average price of BUYING PARKING in each CABA neighborhood -- C1
+
+SELECT l3, AVG(price) AS AVGprice, COUNT(l3), AVG(surface_total)
+FROM zonaprop_data
+WHERE operation_type = 'Venta' 
+AND l3 IS NOT NULL 
+AND property_type LIKE('Cochera')
+AND surface_total < 20
+AND price <  70000 AND price > 1000
+GROUP BY l3
+HAVING AVG(price) IS NOT NULL AND COUNT(l3) >10
+ORDER BY AVGprice DESC;
+
